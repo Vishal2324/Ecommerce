c1c3a67f08403f63f3ac96609e660dded3661cb2
Vishal2324 <bajajvishal8@gmail.com>
2019-01-11 17:36:43 +0530

    “adding”

--- app.js
+++ app.js
@@ -1,51 +1,37 @@
 
-const http = require('http');
+var http = require("http");
+var express = require('express');
+const path = require('path');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+var url = require('url'); 
+var querystring = require('querystring');
 
-const port=process.env.PORT || 3000
+var prt = process.env.PORT || 3000;
+process.env.NODE_ENV = 'production';
+var server = app.listen(prt, function () {
+   var host = server.address().address
+   var port = server.address().port   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Access-Control-Allow-Headers, X-Requested-With, Content-Type, Accept, x-access-token");
+  next();
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+
+app.use(express.static(path.resolve(__dirname,'./client/build')));
+
+app.get('*',function(req, res){
+  res.sendFile(path.resolve(__dirname,'./client/build','index.html'));
 });
-
-// var http = require("http");
-// var express = require('express');
-// const path = require('path');
-// var app = express();
-// var mysql      = require('mysql');
-// var bodyParser = require('body-parser');
-// var url = require('url'); 
-// var querystring = require('querystring');
-
-// var prt = process.env.PORT || 3000;
-// process.env.NODE_ENV = 'production';
-// var server = app.listen(prt, function () {
-//    var host = server.address().address
-//    var port = server.address().port   
-//    console.log("Example app listening at http://%s:%s", host, port)
-// })
-
-// app.use( bodyParser.json() );       // to support JSON-encoded bodies
-// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-//   extended: true
-// }));
-
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, Access-Control-Allow-Headers, X-Requested-With, Content-Type, Accept, x-access-token");
-//   next();
-// });
-
-
-// app.use(express.static(path.resolve(__dirname,'./react_spa/build')));
-
-// app.get('*',function(req, res){
-//   res.sendFile(path.resolve(__dirname,'./react_spa/build','index.html'));
-// });
 
